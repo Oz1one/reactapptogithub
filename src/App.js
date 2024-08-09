b870fdf81270a540ab2ca08e7f42ef416d4d3ac1
Oz1one <48507452+Oz1one@users.noreply.github.com>
2024-08-09 17:33:43 +0530

    second Last commit post 10 videos and old link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Home3 from './Pages/home3.js'
 import Home4 from './Pages/home4.js'
 import Home5 from './Pages/home5.js'
 import Home6 from './Pages/home6.js'
+import Home7 from './Pages/home7.js'
+import Home8 from './Pages/home8.js'
+import Home9 from './Pages/home9.js'
+import Home10 from './Pages/home10.js'
+
 
 import { BrowserRouter as Router, Routes,useRoutes, Route, Link } from "react-router-dom";
 
@@ -17,7 +22,13 @@ const App = () => {
       { path: '/Blessing3', element: <Home3 />},
       { path: '/Blessing4', element: <Home4 />},
       { path: '/Blessing5', element: <Home5 />},
-      { path: '/Blessing6', element: <Home6 />}
+      { path: '/Blessing6', element: <Home6 />},
+      { path: '/Blessing7', element: <Home7 />},
+      { path: '/Blessing8', element: <Home8 />},
+      { path: '/Blessing9', element: <Home9 />},
+      { path: '/Blessing10', element: <Home10 />}
+
+
 
   ]);
 
---
+++ src/Pages/home7.js
@@ -0,0 +1,51 @@
+
+import "../Styling/homeMain.css";
+import React, { useRef, useState } from 'react';
+import video from "../Assets/Video7.mp4";
+import "../Styling/videoPlayerStyle.css";
+import { Link } from 'react-router-dom';
+
+ 
+ 
+
+
+export default function Home7() {
+  
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  };
+  return (
+    <main>
+      <div className="container">
+      
+
+        <div className="video-content">
+        
+        <div className="video-player-container">
+      <video ref={videoRef} className='videoPlayerStyle' >
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      
+      <div className='controls'>
+        <button onClick={togglePlay}>
+          {isPlaying ? '♡＼(￣▽￣)／♡' : '♡( ◡‿◡ )'}
+        </button>
+      </div>
+      <Link to="/Blessing8">Next</Link>
+    </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
